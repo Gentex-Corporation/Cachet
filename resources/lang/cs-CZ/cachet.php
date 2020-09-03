adad9a0a6c7fe9d50c12cbc71d8e7cb21b6a286a
Jake Kemme <jdkemme@users.noreply.github.com>
2020-09-03 14:54:34 -0400

    Revert "New translations cachet.php (Czech)"
    This reverts commit d454b99e9de33e36f024f9c07795f2fa89481bbf.

--- resources/lang/cs-CZ/cachet.php
+++ resources/lang/cs-CZ/cachet.php
@@ -34,7 +34,7 @@ return [
         'stickied'     => 'Stickied Incidents',
         'scheduled'    => 'Maintenance',
         'scheduled_at' => ', plánované na :timestamp',
-        'posted'       => 'Posted :timestamp by :username',
+        'posted'       => 'Posted :timestamp',
         'posted_at'    => 'Posted at :timestamp',
         'status'       => [
             1 => 'Zkoumání příčiny',
